9e7f1c92cd36fefe5e7b3a772bab55a62b43fc3d
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-02-01 14:53:20 -0300

    add background and simple moviments

--- core/src/com/marcosviniciusferreira/flappybird/Jogo.java
+++ core/src/com/marcosviniciusferreira/flappybird/Jogo.java
@@ -4,18 +4,26 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Jogo extends ApplicationAdapter {
 
     private SpriteBatch batch;
     private Texture passaro;
+    private Texture fundo;
+
+    //Atributos de configurações
+    private float larguraDispositivo;
+    private float alturaDispositivo;
 
     @Override
     public void create() {
 
         batch = new SpriteBatch();
         passaro = new Texture("passaro1.png");
+        fundo = new Texture("fundo.png");
+
+        larguraDispositivo = Gdx.graphics.getWidth();
+        alturaDispositivo = Gdx.graphics.getHeight();
 
     }
 
@@ -23,6 +31,7 @@ public class Jogo extends ApplicationAdapter {
     public void render() {
 
         batch.begin();
+        batch.draw(fundo, 0, 0, larguraDispositivo, alturaDispositivo);
         batch.draw(passaro, 300, 800);
         batch.end();
 
