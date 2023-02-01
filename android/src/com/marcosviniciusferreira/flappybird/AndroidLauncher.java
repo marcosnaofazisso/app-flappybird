c0ec41d817d92abeb80b1eb3162188c9f1ae9347
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-02-01 10:55:42 -0300

    initial commit

---
+++ android/src/com/marcosviniciusferreira/flappybird/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.marcosviniciusferreira.flappybird;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.marcosviniciusferreira.flappybird.Jogo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Jogo(), config);
+	}
+}
