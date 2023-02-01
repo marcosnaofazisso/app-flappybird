96e05ecbffb3a64293d6762f95ecf01f5d15c685
Marcos Vinicius Ferreira <marcosferreiraf22@gmail.com>
2023-02-01 11:22:07 -0300

    add bird sprite, ready to set moves

--- core/src/com/marcosviniciusferreira/flappybird/Jogo.java
+++ core/src/com/marcosviniciusferreira/flappybird/Jogo.java
@@ -1,31 +1,35 @@
 package com.marcosviniciusferreira.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Jogo extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private SpriteBatch batch;
+    private Texture passaro;
+
+    @Override
+    public void create() {
+
+        batch = new SpriteBatch();
+        passaro = new Texture("passaro1.png");
+
+    }
+
+    @Override
+    public void render() {
+
+        batch.begin();
+        batch.draw(passaro, 300, 800);
+        batch.end();
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
